d70889e952630e4fa4ed81d9e34ac037f81e0591
Leon Starr <leon_starr@modelint.com>
2022-10-08 18:02:31 -0700

    Started new branch to add transaction feature

--- PyRAL/database.py
+++ PyRAL/database.py
@@ -4,9 +4,7 @@ database.py -- TclRAL Database
 import logging
 import tkinter
 
-# RPATH = "/Users/starr/SDEV/TclRAL"
 logger = logging.getLogger(__name__)
 db = tkinter.Tcl()
-# db.eval(f'::tcl::tm::path add {RPATH}')
-db.eval("source init_TclRAL.tcl")
+db.eval("source tcl_scripts/init_TclRAL.tcl")
 logger.info("Created a TclRAL db")
