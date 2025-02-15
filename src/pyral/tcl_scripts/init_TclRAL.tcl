f41ad7878fbd3505f5874d14e4df9ccdfcddab39
Leon Starr <leon_starr@modelint.com>
2025-02-14 16:33:07 -0800

    Updated manifest and added tclral files directly so no external path ref anymore

--- src/pyral/tcl_scripts/init_TclRAL.tcl
+++ src/pyral/tcl_scripts/init_TclRAL.tcl
@@ -1,7 +1,23 @@
 # You need to download the tcl ral and ralutil packages for your platform
 # See README.md for a link to the tclral site
 # Then you must edit the line below so that it is the path to where those packages reside on your platform
-set RPATH /Users/starr/SDEV/TclRAL
+#set RPATH /Users/starr/SDEV/TclRAL
+# Get the directory where the script is located
+
+set BASE_DIR [file dirname [info script]]
+
+# Determine system architecture
+set arch [exec uname -m]
+
+# Set the appropriate subdirectory based on architecture
+if {$arch eq "arm64"} {
+    set RPATH [file join $BASE_DIR "MacSilicon"]
+} else {
+    set RPATH [file join $BASE_DIR "MacIntel"]
+}
+
+# Debugging output
+#puts "RPATH set to: $RPATH"
 
 ::tcl::tm::path add $RPATH
 
