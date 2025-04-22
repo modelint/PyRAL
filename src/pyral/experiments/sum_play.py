48291ffb8513619bb8a8acd0fa275ea19c832cce
Leon Starr <leon_starr@modelint.com>
2025-04-21 22:58:27 -0700

    Success - relational expression determines downstream execution properly

--- src/pyral/experiments/sum_play.py
+++ src/pyral/experiments/sum_play.py
@@ -66,6 +66,7 @@ class SumTest:
                             Action_i(ID="ACTN8"),
                         ], svar_name="xactions")
         Relation.print(db=fdb, variable_name="xactions")
+        Relation.rename(db=fdb, names={"ID":"From_action"}, svar_name="xfrom")
 
         Relation.join(db=fdb, rname1="Flow_Dependency", rname2="xactions", attrs={"From_action":"ID"}, svar_name="iflows")
         Relation.print(db=fdb, variable_name="iflows")
@@ -74,14 +75,33 @@ class SumTest:
         Relation.project(db=fdb, attributes=("To_action",), relation="iflows", svar_name="downstream")
         Relation.print(db=fdb, variable_name="downstream")
 
+        Relation.raw(db=fdb, cmd_str="relation join $Flow_Dependency $downstream", svar_name="join_inputs")
+        # Relation.join(db=fdb, rname1="Flow_Dependency", rname2="downstream", svar_name="join_inputs")
+        Relation.print(db=fdb, variable_name="join_inputs")
+
         R = f"To_action:<ACTN9>"
         Relation.restrict(db=fdb, relation='downstream', restriction=R, svar_name="ds_tup")
         Relation.print(db=fdb, variable_name="ds_tup")
+        # Relation.join(db=fdb, rname1="Flow_Dependency", rname2="ds_tup", svar_name="j")
+        Relation.raw(db=fdb, cmd_str="relation join $Flow_Dependency $ds_tup", svar_name="j")
+        Relation.print(db=fdb, variable_name="j")
+        #
+        # Relation.join(db=fdb, rname2="Flow_Dependency", rname1="ds_tup")
+        # Relation.project(db=fdb, attributes=("From_action",), svar_name="up1")
+        # Relation.print(db=fdb, variable_name="up1")
+        # Relation.raw(db=fdb, cmd_str="relation rename $Flow_Dependency From_action Hello", svar_name="test")
+        # Relation.print(db=fdb, variable_name="test")
 
-        Relation.join(db=fdb, rname2="Flow_Dependency", rname1="ds_tup")
-        Relation.project(db=fdb, attributes=("From_action",), svar_name="up1")
-        Relation.print(db=fdb, variable_name="up1")
-        Relation.raw(db=fdb, cmd_str="relation rename $Flow_Dependency From_action Hello", svar_name="test")
-        Relation.print(db=fdb, variable_name="test")
+        pass
+        # Relation.project(db=fdb, relation="join_inputs", attributes=("To_action",), svar_name="to")
+        # Relation.raw(db=fdb, cmd_str="relation summarize $join_inputs $to s numinputs int {[relation cardinality $s]}", svar_name="c")
+        Relation.raw(db=fdb, cmd_str="relation summarizeby $join_inputs To_action s canexecute boolean {[relation is [relation project [relation join $join_inputs $s] From_action] subsetof [relation rename $xactions ID From_action]]}", svar_name="c")
+        # Relation.raw(db=fdb, cmd_str="relation summarizeby $join_inputs To_action s numinputs int {[relation cardinality $s]} xenable boolean {[relation is $s subsetof $xfrom]}", svar_name="c")
+        # Relation.raw(db=fdb, cmd_str="relation summarize $iflows $to s numinputs int {[relation cardinality [relation join $s $Flow_Dependency]]}", svar_name="c")
+        # result = Relation.raw(db=fdb, cmd_str="relation summarizeby $iflows To_action s numinputs int {[relation cardinality [relation join $s $Flow_Dependency]]}", svar_name="c")
+        # result = Relation.raw(db=fdb, cmd_str="relation summarizeby $iflows To_action s numinputs string {[tuple extract $s From_action]}", svar_name="c")
+        pass
+        # Relation.raw(db=fdb, cmd_str="relation summarizeby $iflows To_action s numinputs int {[relation cardinality [relation join $s $iflows]]}", svar_name="c")
+        Relation.print(db=fdb, variable_name="c")
 
 
